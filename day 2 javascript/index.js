93636fc8be1ed8e78acc66a6a96354067725e6ea
Shadow Of Leaf <simokb46@gmail.com>
2023-08-25 00:28:55 +0100

    rewrite code to fully understand it

--- day 2 javascript/index.js
+++ day 2 javascript/index.js
@@ -1,68 +1,59 @@
-//Rock Paper Scissors Game Project//
-console.log("Rock Paper Scissors Game");
+console.log("Rock Paper Scissor Game")
 
-// Function to get a random computer choice
-function getComputerChoice() {
-  var rock = 1;
-  var paper = 2;
-  var scissors = 3;
-  const choices = [rock, paper, scissors];
-  const choice = Math.floor(Math.random() * choices.length);
-  return choices[choice];
+function getBotChoice() {
+var rock = 1
+var paper = 2
+var scissors = 3
+const choices = [rock, paper, scissors ];
+const choice = Math.floor(Math.random() * choices.length);
+return choices[choice];
 }
 
-// Function to determine the winner of a round
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
+function PlayRound(playerSelection, computerSelection) {
+if(playerSelection === computerSelection) {
     return "Tie";
-  } else if (
-    (playerSelection === 1 && computerSelection === 3) ||
-    (playerSelection === 2 && computerSelection === 1) ||
-    (playerSelection === 3 && computerSelection === 2)
-  ) {
-    return "Player";
-  } else {
-    return "Computer";
-  }
+
+} else if
+    ((playerSelection === 1 && computerSelection === 3)
+ || (playerSelection === 2 && computerSelection === 1) 
+ || (playerSelection === 3 && computerSelection === 2))
+ {
+    return "win"
+} else {
+    return "lost"
+    }
 }
 
-// Function to map numeric choice to choice name
-function getChoiceName(choice) {
-  switch (choice) {
-    case 1:
-      return "Rock";
-    case 2:
-      return "Paper";
-    case 3:
-      return "Scissors";
-    default:
-      return "Invalid Choice";
-  }
+function getChoice(choice) {
+    switch(choice) {
+        case 1:
+            return "Rock"
+        case 2: 
+            return "Paper"
+        case 3: 
+            return "Scissors"
+        default:
+            return "Invalid Choice";
+    }
 }
+function game(){
+    const prompt = require('prompt-sync')();
 
-// Function to play the game
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+    const playerchoice = Number(prompt
+        ("Choose 1 for Rock, 2 for Paper and 3 for Scissors"))
+    const playerChoicename = getChoice(playerchoice)
+    const botChoice = getBotChoice();
+    const result = PlayRound(playerchoice, botChoice)
 
-  const prompt = require('prompt-sync')();
-
-  // Round 1
-  const playerChoice1 = Number(prompt("Round 1: Choose: 1 for Rock, 2 for Paper, 3 for Scissors"));
-  const playerChoiceName1 = getChoiceName(playerChoice1);
-  const computerChoice1 = getComputerChoice();
-  const roundResult1 = playRound(playerChoice1, computerChoice1);
-
-  console.log(`Round 1: You choose ${playerChoiceName1}, Computer choose ${getChoiceName(computerChoice1)}`);
-  if (roundResult1 === "Player") {
-    playerScore++;
-    console.log("You win this round!");
-  } else if (roundResult1 === "Computer") {
-    computerScore++;
-    console.log("Computer wins this round!");
-  } else {
-    console.log("It's a tie!");
-  }
+    console.log(`you choose ${playerChoicename} , Computer choose ${getChoice(botChoice)}`) 
+    if (result === "win") {
+        console.log("you win")
+    }
+    else if (result === "lost") {
+        console.log("you lose")
+    } else {
+        console.log("it's a tie!"); 
+    } 
 }
 
 game();
