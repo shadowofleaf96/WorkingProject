8f99c4723a43783ca446d6e40b47aa3896b4d85e
Shadow Of Leaf <simokb46@gmail.com>
2023-08-25 11:39:15 +0100

    fix things

---
+++ day 2 javascript/testing.js
@@ -0,0 +1,8 @@
+let fruits = ["Apple", "Orange", "Banana"];
+fruits.push('Plums', 'Grapes');
+fruits.unshift('Cherry', 'Pineapple');
+fruits.splice(3, 1,'Lemon');
+
+fruits.sort();
+console.log(fruits);
+
