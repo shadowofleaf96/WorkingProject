75fdeebe4217ed2eb880152bb58f6801b7f1212b
Shadow Of Leaf <simokb46@gmail.com>
2023-10-04 23:12:22 +0100

    add day 37 Challenge

---
+++ day 37 challenge/models/user.js
@@ -0,0 +1,29 @@
+// models/user.js
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+const users = [
+  {
+    id: 1,
+    username: "alice",
+    passwordHash: bcrypt.hashSync("user123", saltRounds),
+    role: "user",
+  },
+  {
+    id: 2,
+    username: "admin",
+    passwordHash: bcrypt.hashSync("admin1234", saltRounds),
+    role: "admin",
+  },
+];
+
+function findByUsername(username) {
+  return users.find((user) => user.username === username);
+}
+
+module.exports = {
+  findByUsername,
+  saltRounds, // Export the saltRounds property
+  users, // Export the users array
+};
